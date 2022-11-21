d3573ede911d31daa80232f1cb6a63ae7cdaf17d
Henry Joerg <henry.joerg@gmail.com>
2022-11-21 15:08:16 +0100

    code reformat

--- src/App.js
+++ src/App.js
@@ -1,109 +1,127 @@
 import './App.css';
 import {
-    Button,
-    Card,
-    containerClasses,
-    FormControlLabel,
-    FormGroup,
-    Grid,
-    Slider,
-    Switch,
-    TextField,
-    Typography
-} from "@mui/material";
-import {useState} from "react";
-import CopyToClipboardButton from "./CopyToClipboardButton";
+  Button,
+  Card,
+  FormControlLabel,
+  FormGroup,
+  Slider,
+  Switch,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import CopyToClipboardButton from './CopyToClipboardButton';
 
 function App() {
-    const [buttonContent, setButtonContent] = useState('Generate');
-    const [password, setPassword] = useState('');
-    const [length, setLength] = useState(12);
-    const [upperCase, setUpperCase] = useState(true);
-    const [numbers, setNumbers] = useState(true);
-    const [specialChars, setSpecialChars] = useState(false);
+  const [buttonContent, setButtonContent] = useState('Generate');
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(12);
+  const [upperCase, setUpperCase] = useState(true);
+  const [numbers, setNumbers] = useState(true);
+  const [specialChars, setSpecialChars] = useState(false);
 
-    const generatePassword = () => {
-        let s = 'abcdefghijklmnopqrstuvwxyz';
-        if (numbers) {
-            s += '0123456789'
-        }
-        if (upperCase) {
-            s += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        }
-        if (specialChars) {
-            s += '/-.,[]{}:;^$&%*#+@'
-        }
-        let p = getRandomChars(s);
-        setPassword(p);
-        navigator.clipboard.writeText(p).then(() => {
-        });
-        setButtonIcon();
+  const generatePassword = () => {
+    let s = 'abcdefghijklmnopqrstuvwxyz';
+    if (numbers) {
+      s += '0123456789';
     }
-
-    const setButtonIcon = () => {
-        setButtonContent('✅');
-        delay(1000).then(() => {
-            setButtonContent('Generate');
-        });
+    if (upperCase) {
+      s += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     }
-
-    const getRandomChars = (s) => {
-        let result = '';
-        let characters = s;
-        let charactersLength = characters.length;
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
+    if (specialChars) {
+      s += '/-.,[]{}:;^$&%*#+@';
     }
+    let p = getRandomChars(s);
+    setPassword(p);
+    navigator.clipboard.writeText(p).then(() => {});
+    setButtonIcon();
+  };
 
-    const delay = (time) => {
-        return new Promise(resolve => setTimeout(resolve, time));
+  const setButtonIcon = () => {
+    setButtonContent('✅');
+    delay(1000).then(() => {
+      setButtonContent('Generate');
+    });
+  };
+
+  const getRandomChars = (s) => {
+    let result = '';
+    let characters = s;
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
+    return result;
+  };
 
-    return (
-        <div className="App">
-            <Card>
-                <div className={"center"}>
-                    <h1>PAWOG</h1>
-                </div>
-                <FormGroup>
-                    <Typography id="input-slider" gutterBottom>
-                        Passwortlänge
-                    </Typography>
-                    <Slider
-                        defaultValue={12}
-                        valueLabelDisplay="auto"
-                        aria-labelledby="input-slider"
-                        step={1}
-                        marks
-                        min={8}
-                        max={35}
-                        value={length}
-                        onChange={(event) => setLength(event.target.value)}
+  const delay = (time) => {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  };
 
-                    />
-                    <div className={"flexContainer justifyContentEvenly contentContainer"}>
-                        <FormControlLabel
-                            control={<Switch defaultChecked value={upperCase} onChange={() => setUpperCase(!upperCase)}/>}
-                            label="Grossbuchstaben"/>
-                        <FormControlLabel
-                            control={<Switch defaultChecked value={numbers} onChange={() => setNumbers(!numbers)}/>}
-                            label="Zahlen"/>
-                        <FormControlLabel
-                            control={<Switch value={specialChars} onChange={() => setSpecialChars(!specialChars)}/>}
-                            label="Sonderzeichen"/>
-                    </div>
-                    <div className={"contentContainer"}>
-                        <TextField id="outlined-basic" label="Passwort" variant="outlined" value={password} fullWidth InputProps={{endAdornment: <CopyToClipboardButton password={password}/>}} />
-                    </div>
-                    <div className={"contentContainer flexContainer justifyContentEnd"}>
-                        <Button id="generate" variant="contained" onClick={() => generatePassword()}>{buttonContent}</Button>
-                    </div>
-                </FormGroup>
-            </Card>
+  return (
+    <div className="App">
+      <Card>
+        <div className={'center'}>
+          <h1>PAWOG</h1>
         </div>
-    );
+        <FormGroup>
+          <Typography id="input-slider" gutterBottom>
+            Passwortlänge
+          </Typography>
+          <Slider
+            defaultValue={12}
+            valueLabelDisplay="auto"
+            aria-labelledby="input-slider"
+            step={1}
+            marks
+            min={8}
+            max={35}
+            value={length}
+            onChange={(event) => setLength(event.target.value)}
+          />
+          <div className={'flexContainer justifyContentEvenly contentContainer'}>
+            <FormControlLabel
+              control={
+                <Switch
+                  defaultChecked
+                  value={upperCase}
+                  onChange={() => setUpperCase(!upperCase)}
+                />
+              }
+              label="Grossbuchstaben"
+            />
+            <FormControlLabel
+              control={
+                <Switch defaultChecked value={numbers} onChange={() => setNumbers(!numbers)} />
+              }
+              label="Zahlen"
+            />
+            <FormControlLabel
+              control={
+                <Switch value={specialChars} onChange={() => setSpecialChars(!specialChars)} />
+              }
+              label="Sonderzeichen"
+            />
+          </div>
+          <div className={'contentContainer'}>
+            <TextField
+              id="outlined-basic"
+              label="Passwort"
+              variant="outlined"
+              value={password}
+              fullWidth
+              InputProps={{ endAdornment: <CopyToClipboardButton password={password} /> }}
+            />
+          </div>
+          <div className={'contentContainer flexContainer justifyContentEnd'}>
+            <Button id="generate" variant="contained" onClick={() => generatePassword()}>
+              {buttonContent}
+            </Button>
+          </div>
+        </FormGroup>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
