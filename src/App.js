2de0c5df3a26fe724d188ff5da37cb441bd11727
Henry Joerg <henry.joerg@gmail.com>
2022-12-12 14:40:15 +0100

    on mobile hide side ad and add ad at the bottom

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import {
   Switch,
   TextField,
   Typography,
+  useMediaQuery,
+  useTheme,
 } from '@mui/material';
 import { useState } from 'react';
 import CopyToClipboardButton from './CopyToClipboardButton';
 import Pineapple from './pineapple/Pineapple';
 import adImage from './images/Werbung_2.png';
+import adImageHorizontal from './images/Werbung_1.png';
 
 function App() {
   const [buttonContent, setButtonContent] = useState('Generate');
@@ -62,81 +65,103 @@ function App() {
     return new Promise((resolve) => setTimeout(resolve, time));
   };
 
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <div className={'flexContainer'}>
-      <div className={'adContainer'}>
-        <a href="https://youtu.be/_ycWeIJD9wg?t=2" target={'_blank'}>
-          <img src={adImage} className={'adImage'} />
-        </a>
-      </div>
-      <div className="App">
-        <Card>
-          <div className={'center'}>
-            <h1>PAWOG</h1>
+    <div>
+      <div className={'flexContainer'}>
+        {!isMobile ? (
+          <div className={'adContainer'}>
+            <a href="https://youtu.be/_ycWeIJD9wg?t=2" target={'_blank'}>
+              <img src={adImage} className={'adImage'} />
+            </a>
           </div>
-          <FormGroup>
-            <Typography id="input-slider" gutterBottom>
-              Passwortlänge
-            </Typography>
-            <Slider
-              defaultValue={12}
-              valueLabelDisplay="auto"
-              aria-labelledby="input-slider"
-              step={1}
-              marks
-              min={8}
-              max={35}
-              value={length}
-              onChange={(event) => setLength(event.target.value)}
-            />
-            <div className={'flexContainer justifyContentEvenly contentContainer'}>
-              <FormControlLabel
-                control={
-                  <Switch
-                    defaultChecked
-                    value={upperCase}
-                    onChange={() => setUpperCase(!upperCase)}
-                  />
-                }
-                label="Grossbuchstaben"
-              />
-              <FormControlLabel
-                control={
-                  <Switch defaultChecked value={numbers} onChange={() => setNumbers(!numbers)} />
-                }
-                label="Zahlen"
-              />
-              <FormControlLabel
-                control={
-                  <Switch value={specialChars} onChange={() => setSpecialChars(!specialChars)} />
-                }
-                label="Sonderzeichen"
-              />
+        ) : (
+          ''
+        )}
+        <div className="App">
+          <Card>
+            <div className={'center'}>
+              <h1>PAWOG</h1>
             </div>
-            <div className={'contentContainer'}>
-              <TextField
-                id="outlined-basic"
-                label="Passwort"
-                variant="outlined"
-                value={password}
-                fullWidth
-                InputProps={{ endAdornment: <CopyToClipboardButton password={password} /> }}
+            <FormGroup>
+              <Typography id="input-slider" gutterBottom>
+                Passwortlänge
+              </Typography>
+              <Slider
+                defaultValue={12}
+                valueLabelDisplay="auto"
+                aria-labelledby="input-slider"
+                step={1}
+                marks
+                min={8}
+                max={35}
+                value={length}
+                onChange={(event) => setLength(event.target.value)}
               />
-            </div>
-            <div className={'contentContainer flexContainer justifyContentEnd'}>
-              <Button id="generate" variant="contained" onClick={() => generatePassword()}>
-                {buttonContent}
-              </Button>
-              {passwordCreated ? <Pineapple /> : ''}
-            </div>
-          </FormGroup>
-        </Card>
+              <div className={'flexContainer justifyContentEvenly contentContainer'}>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      defaultChecked
+                      value={upperCase}
+                      onChange={() => setUpperCase(!upperCase)}
+                    />
+                  }
+                  label="Grossbuchstaben"
+                />
+                <FormControlLabel
+                  control={
+                    <Switch defaultChecked value={numbers} onChange={() => setNumbers(!numbers)} />
+                  }
+                  label="Zahlen"
+                />
+                <FormControlLabel
+                  control={
+                    <Switch value={specialChars} onChange={() => setSpecialChars(!specialChars)} />
+                  }
+                  label="Sonderzeichen"
+                />
+              </div>
+              <div className={'contentContainer'}>
+                <TextField
+                  id="outlined-basic"
+                  label="Passwort"
+                  variant="outlined"
+                  value={password}
+                  fullWidth
+                  InputProps={{ endAdornment: <CopyToClipboardButton password={password} /> }}
+                />
+              </div>
+              <div className={'contentContainer flexContainer justifyContentEnd'}>
+                <Button id="generate" variant="contained" onClick={() => generatePassword()}>
+                  {buttonContent}
+                </Button>
+                {passwordCreated ? <Pineapple /> : ''}
+              </div>
+            </FormGroup>
+          </Card>
+        </div>
+        {!isMobile ? (
+          <div className={'adContainer'}>
+            <a href={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={'_blank'}>
+              <img src={adImage} className={'adImage'} />
+            </a>
+          </div>
+        ) : (
+          ''
+        )}
       </div>
-      <div className={'adContainer'}>
-        <a href={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={'_blank'}>
-          <img src={adImage} className={'adImage'} />
-        </a>
-      </div>
+      {isMobile ? (
+        <div className={'horizontalAdContainer'}>
+          <a href={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={'_blank'}>
+            <img src={adImageHorizontal} className={'adImage'} />
+          </a>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
